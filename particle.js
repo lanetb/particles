384e38dda9759160f5097ed29c0720891abcf18b
lanetb <lanetb@dukes.jmu.edu>
2023-02-24 18:23:27 -0500

    particles  working but mouse interaction not

--- particle.js
+++ particle.js
@@ -33,7 +33,74 @@ class Particle{
     draw(){
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
-        ctx.fillStyle = '#FFFFFF';
+        ctx.fillStyle = '#ffffff';
         ctx.fill();
     }
+
+    update(){
+        if (this.x > canvas.width || this.x < 0){
+            this.directionX = -this.directionX;
+        }
+        if (this.y > canvas.height || this.y < 0){
+            this.directionY = -this.directionY;
+        }
+
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+        let distance = Math.sqrt(dx ** 2 + dy ** 2);
+        if (distance < mouse.radius + this.size){
+            if (mouse.x < this.x && this.x < canvas.width - this.size * 10){
+                this.x += 10;
+            }
+            if (mouse.x > this.x && this.x > this.size * 10){
+                this.x -= 10;
+            }
+            if(mouse.y < this.y && this.y < canvas.height - this.size * 10){
+                this.y += 10;
+            }
+            if(mouse.y > this.y && this.y > this.size * 10){
+                this.y -= 10;
+            }
+        }
+        this.x += this.directionX;
+        this.y += this.directionY;
+        this.draw();
+    }
 }
+
+function init(){
+    particleArray = [];
+    let numberOfParticles = (canvas.height * canvas.width) / 9000;
+    for (let i = 0; i < numberOfParticles; i++){
+        let size = (Math.random() * 5) + 1;
+        let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) 
+        + size * 2);
+        let y = (Math.random() * ((innerHeight - size * 2) - (size * 2))
+        + size * 2);
+        let directionX = (Math.random() * 5) - 2.5;
+        let directionY = (Math.random() * 5) - 2.5;
+        let color = '#ffffff';
+
+        particleArray.push(new Particle(x, y, directionX, directionY, size, color));
+    }
+}
+
+function animate(){
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+    for (let i = 0; i < particleArray.length; i++){
+        particleArray[i].update();
+    }
+}
+
+function connect(){
+    for (let a = 0; a < particleArray.length; a++){
+        for (let b = a; b < particleArray.length; b++){
+
+        }
+    }
+}
+
+init();
+animate();
