66ccf395f40e010d16cd8a65175fc15c9de8875a
lanetb <lanetb@dukes.jmu.edu>
2023-02-24 17:38:51 -0500

    added files

---
+++ particle.js
@@ -0,0 +1,39 @@
+const canvas = document.getElementById('particleCanvas');
+const ctx = canvas.getContext('2d');
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+
+// Particles
+let particleArray;
+
+// Get mouse position
+let mouse = {
+    x: null,
+    y: null,
+    radius: (canvas.height/80) * (canvas.width/80)
+}
+
+window.addEventListener('mousemove',
+    function(event){
+        mouse.x = event.x;
+        mouse.y + event.y;
+    }
+);
+
+class Particle{
+    constructor(x, y, directionX, directionY, size, color){
+        this.x = x;
+        this.y = y;
+        this.directionX = directionX;
+        this.directionY = directionY;
+        this.size = size;
+        this.color = color;
+    }
+
+    draw(){
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+        ctx.fillStyle = '#FFFFFF';
+        ctx.fill();
+    }
+}
