3cf96fe4bda5930077951ccbbdf639ec3802ff54
lanetb <lanetb@dukes.jmu.edu>
2023-02-24 19:44:40 -0500

    fixed mouse

--- particle.js
+++ particle.js
@@ -10,13 +10,14 @@ let particleArray;
 let mouse = {
     x: null,
     y: null,
-    radius: (canvas.height/80) * (canvas.width/80)
+    radius: (canvas.height/160) * (canvas.width/160)
 }
 
 window.addEventListener('mousemove',
     function(event){
         mouse.x = event.x;
-        mouse.y + event.y;
+        mouse.y = event.y;
+        console.log(mouse.x, mouse.y);
     }
 );
 
@@ -50,16 +51,16 @@ class Particle{
         let distance = Math.sqrt(dx*dx + dy*dy);
         if (distance < mouse.radius + this.size){
             if (mouse.x < this.x && this.x < canvas.width - this.size * 10){
-                this.x += 10;
+                this.directionX = -this.directionX;
             }
             if (mouse.x > this.x && this.x > this.size * 10){
-                this.x -= 10;
+                this.directionX = -this.directionX;
             }
             if(mouse.y < this.y && this.y < canvas.height - this.size * 10){
-                this.y += 10;
+                this.directionY = -this.directionY;
             }
             if(mouse.y > this.y && this.y > this.size * 10){
-                this.y -= 10;
+                this.directionY = -this.directionY;
             }
         }
         this.x += this.directionX;
@@ -89,10 +90,10 @@ function connect(){
     for (let a = 0; a < particleArray.length; a++){
         for (let b = a; b < particleArray.length; b++){
             let distance = ((particleArray[a].x - particleArray[b])
-            * (particleArray[a].x - particleArray[b].x)
-            + (particleArray[a].y - particleArray[b].y)
+            * (particleArray[a].x - particleArray[b].x))
+            + ((particleArray[a].y - particleArray[b].y)
             * (particleArray[a].y - particleArray[b].y));
-            if (distance < (canvas.width/7 * canvas.height/7)){
+            if (distance < (canvas.width/7) * (canvas.height/7)){
                 ctx.strokeStyle = 'rgba(255,255,255,1)';
                 ctx.lineWidth = 1;
                 ctx.beginPath();
@@ -113,6 +114,15 @@ function animate(){
     }
     connect();
 }
+
+window.addEventListener('resize', 
+    function(){
+        canvas.width = innerWidth;
+        canvas.height = innerHeight;
+        mouse.radius = ((canvas.height/80) * (canvas.height/80));
+        init();
+    }
+)
 
 init();
 animate();
