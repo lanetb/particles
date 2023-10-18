c3190471214c6afb82284a11fe28938aa3d9bba1
lanetb <lanetb@dukes.jmu.edu>
2023-10-18 18:56:08 -0400

    optimized code and made it easier to customize

--- particle.js
+++ particle.js
@@ -4,6 +4,16 @@ ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
 const fps = 60;
+const ballNumModifier = 10000;
+const ballSpeed = 6;
+const ballColor = 'rgba(249,199,94,.5)';
+const ballCol = 'rgba(249,199,94,'
+const lineDistanceModifier = 6;
+const particleType = "star";
+const starPoints = 5;
+const particleSize = 5;
+const dashedLine = [5, 15] //[] for solid line
+const mouseRadius = 5;
 
 // Particles
 let particleArray;
@@ -23,20 +33,51 @@ window.addEventListener('mousemove',
     }
 );
 
+function strokeStar(x, y, r, n, inset) {
+    ctx.save();
+    ctx.beginPath();
+    ctx.translate(x, y);
+    ctx.moveTo(0, 0 - r);
+    for (var i = 0; i < n; i++) {
+        ctx.rotate(Math.PI / n);
+        ctx.lineTo(0, 0 - (r * inset));
+        ctx.rotate(Math.PI / n);
+        ctx.lineTo(0, 0 - r);
+    }
+    ctx.closePath();
+    ctx.fill();
+    ctx.restore();
+}
+
 class Particle{
-    constructor(x, y, directionX, directionY, size, color){
+    constructor(x, y, directionX, directionY, size, color, points){
         this.x = x;
         this.y = y;
         this.directionX = directionX;
         this.directionY = directionY;
         this.size = size;
+        this.points = points;
         this.color = color;
     }
 
     draw(){
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
-        ctx.fillStyle = '#ffffff';
+        switch (particleType) {
+            case "circle":
+                ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+                break;
+            case "star":
+                strokeStar(this.x, this.y, this.size, starPoints, 0.5);
+                break;
+            case "starrand":
+                strokeStar(this.x, this.y, this.size, this.points, 0.5);
+                break;
+            default:
+                ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+                break;
+        }
+        
+        ctx.fillStyle =  ballColor;
         ctx.fill();
     }
 
@@ -52,20 +93,20 @@ class Particle{
         let dy = mouse.y - this.y;
         let distance = Math.sqrt(dx*dx + dy*dy);
         if (distance < mouse.radius + this.size){
-            if (mouse.x < this.x && this.x < canvas.width - this.size * 10){
-                this.x += 5;
+            if (mouse.x < this.x && this.x < canvas.width - this.size * mouseRadius){
+                this.x += mouseRadius;
                 this.directionX = -this.directionX;
             }
             if (mouse.x > this.x && this.x > this.size * 10){
-                this.x -= 5;
+                this.x -= mouseRadius;
                 this.directionX = -this.directionX;
             }
             if(mouse.y < this.y && this.y < canvas.height - this.size * 10){
-                this.y += 5;
+                this.y += mouseRadius;
                 this.directionY = -this.directionY;
             }
             if(mouse.y > this.y && this.y > this.size * 10){
-                this.y -= 5;
+                this.y -= mouseRadius;
                 this.directionY = -this.directionY;
             }
         }
@@ -77,18 +118,19 @@ class Particle{
 
 function init(){
     particleArray = [];
-    let numberOfParticles = ((canvas.height * canvas.width) / 6500) + 10;
+    let numberOfParticles = ((canvas.height * canvas.width) / ballNumModifier) + 10;
     for (let i = 0; i < numberOfParticles; i++){
-        let size = (Math.random() * 3) + 2;
+        let size = (Math.random() * particleSize) + 2;
         let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) 
         + size * 2);
         let y = (Math.random() * ((innerHeight - size * 2) - (size * 2))
         + size * 2);
-        let directionX = (Math.random() * 3) - 1.5;
-        let directionY = (Math.random() * 3) - 1.5;
-        let color = '#ffffff';
+        let directionX = (Math.random() * ballSpeed) - ballSpeed/2;
+        let directionY = (Math.random() * ballSpeed) - ballSpeed/2;
+        let color = ballColor;
+        let points = Math.floor(Math.random() * 10) + 5;
 
-        particleArray.push(new Particle(x, y, directionX, directionY, size, color));
+        particleArray.push(new Particle(x, y, directionX, directionY, size, color, points));
     }
 }
 
@@ -100,11 +142,11 @@ function connect(){
             * (particleArray[a].x - particleArray[b].x))
             + ((particleArray[a].y - particleArray[b].y)
             * (particleArray[a].y - particleArray[b].y));
-            if (distance < (canvas.width/6) * (canvas.height/6) + 20000){
-                opac = 1 - (distance/15000);
-                ctx.strokeStyle = 'rgba(255,255,255,' + opac + ')';
+            if (distance < (canvas.width/lineDistanceModifier) * (canvas.height/lineDistanceModifier)){
+                opac = 1 - (distance/(15000-1000));
+                ctx.strokeStyle = ballCol + opac + ')';
                 ctx.lineWidth = 1;
-                ctx.setLineDash([5, 5]);
+                ctx.setLineDash(dashedLine);
                 ctx.beginPath();
                 ctx.moveTo(particleArray[a].x, particleArray[a].y);
                 ctx.lineTo(particleArray[b].x, particleArray[b].y);
@@ -117,7 +159,7 @@ function connect(){
 function animate(){
     setTimeout(() => {
     requestAnimationFrame(animate);
-    }, 1000 / fps);
+    }, 100 / fps);
     ctx.clearRect(0, 0, innerWidth, innerHeight);
 
     for (let i = 0; i < particleArray.length; i++){
